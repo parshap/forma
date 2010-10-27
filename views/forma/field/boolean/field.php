2879902ca0c109856b6a671e585f856d5125d5e3
parshap <supster+github@gmail.com>
2010-10-27 05:17:48 -0700

    fixing boolean field view

--- views/forma/field/boolean/field.php
+++ views/forma/field/boolean/field.php
@@ -1,11 +1,10 @@
 <div class="forma-field forma-field-checkbox <?php if ($field->is_invalid) : ?>invalid<?php endif; ?>">
 	<div class="forma-label">
-		
-	$attributes['lines'] = 40
+		<?php echo Form::label($field->attributes['id'], __($field->label)) ?>
 	</div>
 
 	<div class="forma-input">
-		<?php echo Form::label($field->attributes['id'], __($field->label)) ?>
+		<?php echo Form::input($field->name, $field->value, $field->attributes) ?>
 	</div>
 </div>
 
