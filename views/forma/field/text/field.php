63855614cb0a5418a1a34185fa6b02bc9d4bd453
parshap <supster+github@gmail.com>
2010-10-27 05:18:38 -0700

    adding custom field view for text fields so that word count requirements can be displayed

---
+++ views/forma/field/text/field.php
@@ -0,0 +1,15 @@
+<div class="forma-field <?php if ($field->is_invalid) : ?>invalid<?php endif; ?>">
+	<div class="forma-label" <?php if ($field->hide_label) : ?>style="display: none"<?php endif; ?>>
+		<?php echo $label ?>
+	</div>
+
+	<div class="forma-input">
+		<?php echo $input ?>
+	</div>
+	
+	<div class="forma-extra">
+		<?php if (isset($field->rules['max_words'])) : ?>
+			<?= reset($field->rules['max_words']) ?> words max
+		<?php endif; ?>
+	</div>
+</div>
